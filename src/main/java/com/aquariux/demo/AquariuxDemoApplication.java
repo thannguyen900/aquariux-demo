dab5dc56463a44d9f240cce86c6b9b50563bc6f7
thannguyen900 <nguyenthan900@gmail.com>
2026-03-19 22:19:56 +0700

    initial commit

---
+++ src/main/java/com/aquariux/demo/AquariuxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aquariux.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariuxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariuxDemoApplication.class, args);
+	}
+
+}
